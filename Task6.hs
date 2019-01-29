687393ee3567c0b8b4b11cc4ba4d1d55460c7bb8
AL'X&R AGAKAKSKAGESH GODX <45367895+factsar@users.noreply.github.com>
2019-01-29 11:02:45 -0800

    Update Task6.hs

--- Task6.hs
+++ Task6.hs
@@ -20,7 +20,9 @@ leftestLT t@(Fork hv pt lt rt) | otherwise    = leftestLT lt
 
 insert_ltLT :: LinkedTree a -> LinkedTree a -> LinkedTree a
 insert_ltLT EmptyLT lt_of_rt = lt_of_rt
-insert_ltLT lt lt_of_rt@(Fork hv' pt' lt' rt') = Fork hv' pt' lt rt'
+insert_ltLT lt lt_of_rt@(Fork hv' pt' lt' rt') = res
+    where
+        res = Fork hv' pt' (spLT lt res) (spLT rt' res)
 
 concLT :: LinkedTree a -> LinkedTree a -> LinkedTree a
 concLT lt rt = insert_ltLT lt (leftestLT rt)
@@ -36,19 +38,36 @@ insert t iv = res (tsLT t) iv EmptyLT
     where
         res EmptyLT iv lp = (Fork iv lp EmptyLT EmptyLT)
         res ct@(Fork hv pt lt rt) iv lp
-            | iv < hv  = (Fork hv pt (res lt iv ct) rt)
-        res ct@(Fork hv pt lt rt) iv lp
-            | iv > hv  = (Fork hv pt lt (res rt iv ct))
-        res ct@(Fork hv pt lt rt) iv lp
-            | iv == hv = (Fork hv pt lt rt)
+            | iv < hv  = lres 
+            | iv > hv  = rres
+            | iv == hv = ct
+            where
+                lres = Fork hv lp (res lt iv lp) (spLT rt lres)
+                rres = Fork hv lp (spLT lt rres) (res rt iv lp)
 
 remove :: (Eq a) => (Ord a) => LinkedTree a -> a -> LinkedTree a
-remove t lfv = res (tsLT t) lfv
+remove t lfv = res (tsLT t) lfv EmptyLT
     where
-        res EmptyLT _ = EmptyLT
-        res t@(Fork hv pt lt rt) lfv | hv < lfv  = res lt lfv
-        res t@(Fork hv pt lt rt) lfv | hv > lfv  = res rt lfv
-        res t@(Fork hv pt lt rt) lfv | hv == lfv = concLT lt rt
+        res EmptyLT _ _ = EmptyLT
+        res t@(Fork hv pt lt rt) lfv lp
+            | hv < lfv  = lres 
+            | hv > lfv  = rres
+            | hv == lfv = concLT lt rt
+            where
+                lres = Fork hv lp (res lt lfv lres) (spLT rt lres) 
+                rres = Fork hv lp (spLT lt rres) (res rt lfv rres)
+
+
+spLT :: LinkedTree a -> LinkedTree a -> LinkedTree a
+spLT EmptyLT _ = EmptyLT
+spLT ct@(Fork hv pt lt rt)  pt_ct = Fork hv pt_ct (spLT lt ct) (spLT rt ct)
+
+
+
+
+
+
 
 -- lp - last parent, hv - have value, pt - parent Tree, lt - left Tree, rt - right Tree
 -- t - Tree, ct - current Tree, lfv - looking for value, iv - input value, lfk - loocking for key
+-- pt_ct - current tree parent, lres - left res, rres - right res
